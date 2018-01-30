4ed6355dbaad6508b01c69e62351d97684578460
colefranz <cole.franz@calgaryscientific.com>
2018-01-29 18:29:43 -0700

    added resume, moved build to docs/ for pages

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,10 @@
-var path = require('path');
-var webpack = require('webpack');
-var HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
+  context: __dirname,
   devServer: {
     contentBase: path.resolve(__dirname),
     compress: true,
@@ -10,7 +12,7 @@ module.exports = {
   },
   entry: './index.js',
   output: {
-    path: path.resolve(__dirname),
+    path: path.join(__dirname, '/docs'),
     filename: 'main.bundle.js'
   },
   plugins: [
@@ -18,22 +20,36 @@ module.exports = {
       title: 'Cole Franz',
       filename: 'index.html',
       template: 'index.ejs'
-    })
+    }),
+    new CopyWebpackPlugin([
+      {
+        from: 'assets/',
+        to: ''
+      }
+    ])
   ],
   module: {
     rules: [
       {
         test: /\.less$/,
         use: [
-          "style-loader", "css-loader", "less-loader", "postcss-loader"
+          'style-loader', 'css-loader', 'less-loader', 'postcss-loader'
       ]
       }, {
         test: /\.jsx$/,
         loader: 'babel-loader',
         query: {
           presets: ['env'],
-          plugins: ["transform-class-properties"]
+          plugins: ['transform-class-properties']
         }
+      }, {
+        test: /\.(png)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {}
+          }
+        ]
       }
     ]
   }
